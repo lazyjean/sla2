45b4f623c47c0cec96590947c68dfac5453cc4e7
Liu Zhen <liuzhen@foxmail.com>
2025-02-22 00:05:31 +0800

    feat: add log interceptor

--- internal/interfaces/grpc/server.go
+++ internal/interfaces/grpc/server.go
@@ -1,8 +1,10 @@
 package grpc
 
 import (
+	"context"
 	"fmt"
 	"net"
+	"time"
 
 	pb "github.com/lazyjean/sla2/api/proto/v1"
 	"github.com/lazyjean/sla2/config"
@@ -12,6 +14,8 @@ import (
 	"github.com/lazyjean/sla2/internal/interfaces/grpc/user"
 	"github.com/lazyjean/sla2/internal/interfaces/grpc/word"
 	"github.com/lazyjean/sla2/pkg/auth"
+	"github.com/lazyjean/sla2/pkg/logger"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -27,7 +31,10 @@ type Server struct {
 
 // NewServer 创建 gRPC 服务器
 func NewServer(userService *service.UserService, wordService *service.WordService, learningService *service.LearningService, courseService *service.CourseService, authSvc auth.JWTServicer, cfg *config.Config) *Server {
-	server := grpc.NewServer()
+	// 创建带有拦截器的 gRPC 服务器
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(loggingInterceptor()),
+	)
 
 	s := &Server{
 		server:      server,
@@ -49,6 +56,41 @@ func NewServer(userService *service.UserService, wordService *service.WordServic
 	}
 
 	return s
+}
+
+// loggingInterceptor 创建日志拦截器
+func loggingInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		log := logger.GetLogger(ctx)
+		// 记录请求开始时间
+		start := time.Now()
+
+		// 记录请求信息
+		log.Info("Request", zap.String("method", info.FullMethod), zap.Any("payload", req))
+
+		// 处理请求
+		resp, err := handler(ctx, req)
+
+		// 计算处理时间
+		duration := time.Since(start)
+
+		// 记录响应信息
+		if err != nil {
+			log.Info("Response",
+				zap.String("method", info.FullMethod),
+				zap.Error(err),
+				zap.Duration("duration", duration),
+			)
+		} else {
+			log.Info("Response",
+				zap.String("method", info.FullMethod),
+				zap.Any("payload", resp),
+				zap.Duration("duration", duration),
+			)
+		}
+
+		return resp, err
+	}
 }
 
 // Start 启动 gRPC 服务器
