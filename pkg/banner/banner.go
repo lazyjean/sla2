3c9d548eecb803b2a3f698cb4298119d461da884
liuzhen <lazyjean@foxmail.com>
2025-03-30 19:47:55 +0800

    fix: startup banner panic

--- pkg/banner/banner.go
+++ pkg/banner/banner.go
@@ -20,8 +20,8 @@ const banner = `
 // PrintBanner 打印启动 banner
 func PrintBanner(version string) {
 	// 获取终端宽度
-	width := 80
-	if w, _, err := term.GetSize(0); err == nil {
+	width := 80 // 默认宽度
+	if w, _, err := term.GetSize(0); err == nil && w > 0 {
 		width = w
 	}
 
@@ -34,13 +34,25 @@ func PrintBanner(version string) {
 		}
 	}
 
+	// 确保 banner 不会超出终端宽度
+	if maxWidth > width {
+		maxWidth = width
+	}
+
 	// 计算居中所需的空格
 	padding := (width - maxWidth) / 2
+	if padding < 0 {
+		padding = 0
+	}
 
 	// 打印 banner
 	fmt.Println("\033[36m") // 设置颜色为青色
 	for _, line := range bannerLines {
 		if line != "" {
+			// 如果行太长，截断它
+			if len(line) > width {
+				line = line[:width]
+			}
 			fmt.Printf("%s%s\n", strings.Repeat(" ", padding), line)
 		}
 	}
@@ -55,6 +67,9 @@ func PrintBanner(version string) {
 	// 打印 profile 信息
 	infoLine := fmt.Sprintf("Profile: %s", profile)
 	infoPadding := (width - len(infoLine)) / 2
+	if infoPadding < 0 {
+		infoPadding = 0
+	}
 	fmt.Printf("%s%s\n", strings.Repeat(" ", infoPadding), infoLine)
 
 	// 打印分隔线
